c79154d9e47c89e0e9d05e9053f171732e2ac14c
DK BOTx TG <100774327+DKBOTx@users.noreply.github.com>
2022-05-07 01:45:51 +0530

    Update video_converter.py

--- plugins/video_converter.py
+++ plugins/video_converter.py
@@ -50,7 +50,7 @@ async def convert_to_video(bot, update):
             await update.reply_text(
                 text="**Please Join My Update Channel Before Using Me..**",
                 reply_markup=InlineKeyboardMarkup([
-                    [ InlineKeyboardButton(text="Join My Updates Channel", url=f"https://t.me/{update_channel}")]
+                    [ InlineKeyboardButton(text="ᴊᴏɪɴ ᴍʏ ᴄʜᴀɴɴᴇʟ", url=f"https://t.me/{update_channel}")]
               ])
             )
             return  
