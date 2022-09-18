ffbc3077983c057ba9f0c8c9c7dc9f2ef0868155
peraltaespectri <113333418+peraltaespectri@users.noreply.github.com>
2022-09-18 08:39:13 -0300

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,42 +1,42 @@
 import os
 
 class Config(object):
-    # get a token from @BotFather
+    # obter um token de @BotFather
     TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-    # The Telegram API things
+    # As coisas da API do Telegram
     APP_ID = int(os.environ.get("APP_ID", 12345))
     API_HASH = os.environ.get("API_HASH")
-    # Update channel for Force Subscribe
+    # Atualizar canal para Force Subscribe
     UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "")
-    # log channel
+    # canal de registro
     #LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "")
-    # Get these values from my.telegram.org
+    # Obtenha esses valores em my.telegram.org
     CHAT_ID = os.environ.get("CHAT_ID", "")
-    # Array to store users who are authorized to use the bot
+    # Variedade para armazenar usuários autorizados a usar o bot
     AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
-    # Banned Unwanted Members..
+    # Membros indesejados banidos..
     BANNED_USERS = []
-    # the download location, where the HTTP Server runs
+    # o local de download, onde o servidor HTTP é executado
     DOWNLOAD_LOCATION = "./DOWNLOADS"
-    # Telegram maximum file upload size
+    # Tamanho máximo de upload de arquivo do telegrama
     MAX_FILE_SIZE = 50000000
     TG_MAX_FILE_SIZE = 2097152000
     FREE_USER_MAX_FILE_SIZE = 50000000
-    # chunk size that should be used with requests
+    # tamanho do bloco que deve ser usado com solicitações
     CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
-    # default thumbnail to be used in the videos
+    # miniatura padrão a ser usada nos vídeos
     DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90")
-    # proxy for accessing youtube-dl in GeoRestricted Areas
-    # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
+    # proxy para acessar o youtube-dl em áreas georrestritas
+    # Obtenha seu próprio proxy de https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
     HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
     # https://t.me/hevcbay/951
     OUO_IO_API_KEY = ""
-    # maximum message length in Telegram
+    # comprimento máximo da mensagem no Telegram
     MAX_MESSAGE_LENGTH = 4096
-    # set timeout for subprocess
+    # definir tempo limite para o subprocesso
     PROCESS_MAX_TIMEOUT = 3600
-    # watermark file
+    # arquivo de marca d'água
     DEF_WATER_MARK_FILE = ""
-    # Database url
+    # URL do banco de dados
     DB_URI = os.environ.get("DATABASE_URL", "")
     
